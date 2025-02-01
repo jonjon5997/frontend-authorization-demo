1ef4662f520ed7735d3c8f36c24c46ab4378e5b8
Jonathan Sanfilippo <jonjon5997@gmail.com>
2025-02-01 14:29:00 -0500

    redirect users

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Ducks from "./Ducks";
 import Login from "./Login";
 import MyProfile from "./MyProfile";
 import Register from "./Register";
 import "./styles/App.css";
+import { useState } from "react";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <Routes>
       <Route path="/ducks" element={<Ducks />} />
@@ -24,6 +27,16 @@ function App() {
           <div className="registerContainer">
             <Register />
           </div>
+        }
+      />
+      <Route
+        path="*"
+        element={
+          isLoggedIn ? (
+            <Navigate to="/ducks" replace />
+          ) : (
+            <Navigate to="/login" replace />
+          )
         }
       />
     </Routes>
